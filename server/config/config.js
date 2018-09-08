ae1643faa860c6464e467ef39087a2ec31be288a
Alex <akeymy4@gmail.com>
2018-09-07 22:34:53 -0400

    rest-server, users

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,15 @@
 // PUERTO
 // ======================
 process.env.PORT = process.env.PORT || 3000
+
+// ======================
+// ENTORNO
+// ======================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
+
+// ======================
+// Base de DATOS
+// ======================
+process.env.URLDB = (process.env.NODE_ENV === 'dev') ? 'mongodb://cafe-user:abc1234@ds149742.mlab.com:49742/cafe' : 'mongodb://localhost/cafe'
+// process.env.URLDB = 'mongodb://cafe-user:abc1234@ds149742.mlab.com:49742/cafe'
--- server/server.js
+++ server/server.js
@@ -1,7 +1,9 @@
 require('./config/config')
+require('colors')
 
 const express = require('express')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
 
 const app = express()
 
@@ -11,34 +13,14 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
-app.get('/user', (req, res) => {
-  res.json({
-    user: 'alex'
-  })
-})
+// usando las rutas configuradas
+app.use(require('./routes/users'))
 
-app.delete('/user', (req, res) => {
-  res.send('esto es delete')
-})
-
-app.post('/user', (req, res) => {
-  let body = req.body
-
-  if (body.nom === undefined) {
-    res.status(400).json({
-      ok: false,
-      message: 'Error, no se hallo la variable nom'
-    })
-  } else {
-    res.json(body)
+mongoose.connect(process.env.URLDB, (err, resp) => {
+  if (err) {
+    throw err
   }
-})
-
-app.put('/user/:id', (req, res) => {
-  let id = req.params.id
-  res.json({
-    id
-  })
+  console.log('Mongo corriendo de forma correcta'.green)
 })
 
 app.listen(process.env.PORT, () => console.log(`escuchando por el puerto ${process.env.PORT}`))
