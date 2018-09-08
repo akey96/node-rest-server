3f87141bab5e7bfd07cd48fa48308d8355a22f53
Alex <akeymy4@gmail.com>
2018-09-07 22:52:00 -0400

    modified the enviroments of database

--- server/config/config.js
+++ server/config/config.js
@@ -12,5 +12,5 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev'
 // ======================
 // Base de DATOS
 // ======================
-process.env.URLDB = (process.env.NODE_ENV === 'dev') ? 'mongodb://cafe-user:abc1234@ds149742.mlab.com:49742/cafe' : 'mongodb://localhost/cafe'
+process.env.URLDB = (process.env.NODE_ENV === 'dev') ? 'mongodb://localhost/cafe' : 'mongodb://cafe-user:abc1234@ds149742.mlab.com:49742/cafe'
 // process.env.URLDB = 'mongodb://cafe-user:abc1234@ds149742.mlab.com:49742/cafe'
